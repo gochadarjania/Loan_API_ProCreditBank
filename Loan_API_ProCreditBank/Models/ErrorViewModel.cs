0d9aedca2f4035c4776d17d6d6b1b9cdeef6a967
Gocha Darjania <gocha.darjania@procreditbank.ge>
2022-10-21 17:36:25 +0400

    Test

---
+++ Loan_API_ProCreditBank/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Loan_API_ProCreditBank.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
